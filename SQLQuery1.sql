eeb5aaf37bf8a727549e370c378d1a12c2236901
Gabriela la Cruz <gccm_8@hotmail.com>
2025-07-16 19:02:04 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,41 @@
+--una linea de comentario 
+USE Neptuno
+
+--Las sentencias SQL que muestran datos, son las que empiezan por SELECET y no los modifica 
+--es de solo lectura. La sentencia SELECT tiene 2 partes 
+-- que son sujeto y predicado, el sujeto es la sentencia hasta el from
+-- el predicado despues del from 
+
+SELECT *
+FROM Pedidos;
+
+SELECT Cargo, Destinatario
+FROM Pedidos;
+
+SELECT Destinatario as Cliente 
+FROM Pedidos;
+
+SELECT * 
+FROM Pedidos
+ORDER BY Destinatario DESC;
+
+SELECT *
+FROM Pedidos
+WHERE PaísDestinatario = 'Alemania';
+
+SELECT *
+FROM Pedidos
+WHERE PaísDestinatario = 'Alemania'
+ORDER BY CiudadDestinatario;
+
+SELECT TOP 5 *
+FROM Pedidos;
+
+-- Si tiene espacio en blancos la tabla, va entre corchetes
+SELECT *
+FROM [Detalles de pedidos];
+
+-- Calcular un campo
+
+SELECT PrecioUnidad, Cantidad, (PrecioUnidad*Cantidad) as Importe
+FROM [Detalles de pedidos];
